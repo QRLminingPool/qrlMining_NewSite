824bb994a4a9d7f1c816788b2cffe09b1df534ac
fr1t2 <fr1t2@protonmail.com>
2018-03-15 23:16:25 -0600

    Addig the NodeJS pages from Cyybers Fork

---
+++ config.js
@@ -0,0 +1,17 @@
+var api = "http://pool.qrlmining.com.com:8117";
+
+var coinUnits = 1000000000;
+
+var poolHost = "pool.qrlmining.com.com";
+
+var irc = "irc.freenode.net/#qrl-pools";
+
+var email = "qrlmining@qrlmining.com";
+
+var cryptonatorWidget = ["QRL-BTC", "QRL-USD", "QRL-EUR", "QRL-GBP"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "https://explorer.theqrl.org/block/";
+
+var transactionExplorer = "https://explorer.theqrl.org/tx/";
