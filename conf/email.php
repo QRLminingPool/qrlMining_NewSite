c86c2f61ef11dbd0709a07a1c716f2b08310cc30
fr1t2 <fr1t2@protonmail.com>
2018-03-21 20:31:35 -0600

    EmailTest

--- conf/email.php
+++ conf/email.php
@@ -2,6 +2,8 @@
 // define variables and set to empty values
 $emailErr = "";
 $email = "";
+$data = "";
+$output = shell_exec('bash /home/ubuntu/.mail/emQRLminingList.sh {$data}');
 function test_input($data)
 {
    $data = trim($data);
@@ -9,17 +11,13 @@ function test_input($data)
    $data = htmlspecialchars($data);
    return $data;
 }
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Set the email var 
-    if(!empty($_POST['email'])) {
-    $email = test_input($_POST['email']);
-    error_log("${email}", 0);
-  }
-// set var to pass email to script, You have to change the directory
-$output = shell_exec('bash /home/ubuntu/.mail/emQRLminingList.sh {$email}');
-// Fire the script
-$output;
-header("Location: /index.html");
-exit;
-}
+
+  $email = test_input($_POST['email']);
+	// set var to pass email to script, You have to change the directory
+	error_log("Sent to ${data}", 0);
+	// Fire the script
+	$output;
+	header("Location: /index.html");
+	exit;
+
 ?>
