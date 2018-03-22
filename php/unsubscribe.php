4f8dff92415c6c8311650ecf6269e29f4540c691
fr1t2 <fr1t2@protonmail.com>
2018-03-22 13:25:32 -0600

    fix index

--- php/unsubscribe.php
+++ php/unsubscribe.php
@@ -1,8 +1,15 @@
-unsubscribe.php
 <?php
 
 
-echo '<div class="simple-subscription-form ">
+echo '
+<div data-closable class="alert-box callout alert">
+  <i class="fa fi-alert"></i> You are going to remove your email from our list. <br>Are you sure?
+<button class="close-button" aria-label="Dismiss alert" type="button" data-close>
+    <span aria-hidden="true">&CircleTimes;</span>
+  </button>
+</div>
+
+<div class="simple-subscription-form ">
             <form  action="conf/Unsubscribe.php" method="post">
               <h4 style="color: #eaa228;"><strong>Leave The Mailing List</strong></h4>
               <div class="input-group-dark">
@@ -18,11 +25,6 @@ echo '<div class="simple-subscription-form ">
               </div>
             </form>
           </div>
-<div data-closable class="alert-box callout alert">
-  <i class="fa fi-alert"></i> You are going to remove your email from our list. <br>Are you sure?
-<button class="close-button" aria-label="Dismiss alert" type="button" data-close>
-    <span aria-hidden="true">&CircleTimes;</span>
-  </button>
-</div>
+
           ';
 ?>          
