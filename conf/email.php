6050435008a8ab0c18fbae4ee244c4f44293c48a
fr1t2 <fr1t2@protonmail.com>
2018-03-22 11:48:21 -0600

    add alert

--- conf/email.php
+++ conf/email.php
@@ -23,8 +23,7 @@ $output;
 
 }
 
-echo "<div data-closable class='alert-box callout success'><i class='fa fi-check'></i> Success! '".$email."' has been added to the list. <button class='close-button' aria-label='Dismiss alert' type='button' data-close><span aria-hidden='true'>&CircleTimes;</span></button></div> ";
-header("Location: /index.html");
+header("Location: /index.html?sent=Yes&email=${email}");
 exit;
 }
 ?>
---
+++ conf/thanks.php
@@ -0,0 +1,3 @@
+<?php
+echo "<div data-closable class='alert-box callout success'><i class='fa fi-check'></i> Success! '".$email."' has been added to the list. <button class='close-button' aria-label='Dismiss alert' type='button' data-close><span aria-hidden='true'>&CircleTimes;</span></button></div> ";
+?>
