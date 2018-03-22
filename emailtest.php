c125ceffa767aac8f5d4d552bd07ad381889abc8
fr1t2 <fr1t2@protonmail.com>
2018-03-21 20:15:12 -0600

    EmailTest

--- emailtest.php
+++ emailtest.php
@@ -1,10 +1,9 @@
-emailtest.php
-
 <?php
-  // The global $_POST variable allows you to access the data sent with the POST method by name
-  // To access the data sent with the GET method, you can use $_GET
-  $email = htmlspecialchars($_POST['email']);
-  $to  = htmlspecialchars($_POST['to']);
+  
 
-  echo  $email, ' ', $to;
+if( $_POST["email"])
+{
+echo "Your Email is: ". $_POST["email"]. "<br />";
+
+}
 ?>
