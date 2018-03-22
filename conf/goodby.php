35b5de9837ecdb744e60629cf2dc384317e8f5cb
fr1t2 <fr1t2@protonmail.com>
2018-03-22 13:08:46 -0600

    fix index

--- conf/goodby.php
+++ conf/goodby.php
@@ -2,6 +2,7 @@
 $email = ($_GET['email']);
 
 echo "<div data-closable class='alert-box callout warning'><i class='fa fi-alert'></i> Oh No! ";
+echo "";
 echo $email;
 echo " has been removed from the list. <button class='close-button' aria-label='Dismiss alert' type='button' data-close><span aria-hidden='true'>&CircleTimes;</span></button></div> ";
 ?>
